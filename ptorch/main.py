2ef5b2925c5234c5e202ac79e8984f2704ccf486
aneskimm <aneskim92@gmail.com>
2023-06-03 18:45:45 -0500

    test edf file read

--- ptorch/main.py
+++ ptorch/main.py
@@ -1,5 +1,10 @@
 #just testing pytorch
 
-import torch
-x = torch.rand(5, 3)
-print(x)
+# %%
+# %%
+import mne
+
+file = "/Users/aneskimm/Desktop/neurals/cnn_schzpna/CNN_schizophrenia/dataset/dataverse_files/s01.edf"
+data = mne.io.read_raw_edf(file, preload=True)
+raw_data = data.get_data()
+
